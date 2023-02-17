bb7fc746ccc869a4a17947f4f206c837aa3d820d
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-02-17 13:18:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,60 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+    int flag = 0;
+    for (int i = 2; i < value; i++)
+        if (value % i == 0) {
+            flag = 1;
+            break;
+        }
+    if (flag == 0) return true;
+    else return false;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+    int flag, k = 0, number = 1;
+    while (k != n) {
+        number++;
+        flag = 0;
+        for (int i = 2; i < number; i++)
+            if (number % i == 0) {
+                flag = 1;
+                break;
+            }
+        if (flag == 0) k++;
+    }
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+    int flag, k = 0;
+    while (k != 1) {
+        value++;
+        flag = 0;
+        for (int i = 2; i < value; i++)
+            if (value % i == 0) {
+                flag = 1;
+                break;
+            }
+        if (flag == 0) k++;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+    int flag, sum = 0, number = 1;
+    while (number != (hbound - 1)) {
+        number++;
+        flag = 0;
+        for (int i = 2; i < number; i++)
+            if (number % i == 0) {
+                flag = 1;
+                break;
+            }
+        if (flag == 0) sum += number;
+    }
+    return sum;
 }
